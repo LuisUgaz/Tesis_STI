865163665d129e768219a79393ccd33ff481fe79
LuisUgaz <luisugaz63@gmail.com>
2026-03-27 10:31:43 -0500

    feat:inicio de proyecto

---
+++ AppGestionUsuario/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppgestionusuarioConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'AppGestionUsuario'
---
+++ AppTutoria/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApptutoriaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'AppTutoria'
